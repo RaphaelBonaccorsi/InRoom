403d2a45a9fd6e1742ea587ee5fc95c898d05094
Raphael Bonaccorsi <rbonaccorsi@stepwise.com.br>
2025-06-03 12:37:07 -0300

    Adding icons to navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
 // app/(tabs)/_layout.tsx
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
           backgroundColor: '#11324e',
@@ -16,11 +17,29 @@ export default function TabLayout() {
         tabBarLabelStyle: {
           fontSize: 14,
           fontWeight: '600',
-          color: 'white',
         },
         tabBarActiveTintColor: '#ffffff',
         tabBarInactiveTintColor: '#aaaaaa',
-      }}
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap;
+
+          switch (route.name) {
+            case 'index':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+            case 'login':
+              iconName = focused ? 'log-in' : 'log-in-outline';
+              break;
+            case 'register':
+              iconName = focused ? 'person-add' : 'person-add-outline';
+              break;
+            default:
+              iconName = 'ellipse';
+          }
+
+          return <Ionicons name={iconName} size={24} color={color} />;
+        },
+      })}
     >
       <Tabs.Screen name="index" options={{ title: 'Início' }} />
       <Tabs.Screen name="login" options={{ title: 'Login' }} />
